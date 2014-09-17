8d2e5382b6251c4a8416a4b810a8bf0868fb2ca6
ruslankus <ruslan@yahoo.com>
2014-09-17 17:07:41 +0300

    добавлен скрипт для аакордиона

--- js/common.js
+++ js/common.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+$(document).ready(function(e) {
+    
+	$('.meniu-item-heading  > a' ).click(function(e) {
+		var panObj = $('#accordion .collapse');
+		
+		panObj.each(function() {
+            if($(this).hasClass('in')){
+				$(this).removeClass('in');	
+			}
+        });
+		
+        
+    });
+	
+});//document ready
